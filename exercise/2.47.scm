d882c862a44211b551361539cd486b600c157e30
kaiser <765934690@qq.com>
2020-09-29 13:55:13 +0800

    2.47.scm

---
+++ exercise/2.47.scm
@@ -0,0 +1,29 @@
+(define frame1 (list 1 2 3))
+
+(car frame1)
+(cadr frame1)
+(caddr frame1)
+
+(define (origin-of-frame1 f)
+    (car f))
+
+(define (edge1-of-frame1 f)
+    (cadr f))
+
+(define (edge2-of-frame1 f)
+    (caddr f))
+
+(define frame2 (cons 1 (cons 2 3)))
+
+(car frame2)
+(cadr frame2)
+(cddr frame2)
+
+(define (origin-of-frame2 f)
+    (car f))
+
+(define (edge1-of-frame2 f)
+    (cadr f))
+
+(define (edge2-of-frame2 f)
+    (cddr f))
